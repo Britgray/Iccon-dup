4fc159c6296d2fb19ee2a8782c1e635184a5bd49
Student 18 <student18@Codes-MacBook-Air-4.local>
2023-10-30 09:05:42 -0400

    pagepait

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Header = () => {
+    
+  return (
+    <div>
+    <header>
+      <div className='menu-trigger'><h1>ICCON</h1></div>
+      <nav> 
+        <ul className='header-nav'>
+          <a href='https://shopiccon.com/'>HOME</a>
+          <a href='https://shopiccon.com/collections'>SHOP</a>
+          <a href='https://shopiccon.com/collections/logo'>ICCON LIVIN </a>
+          <a href='https://shopiccon.com/collections/essential-1'>ESSENTIAL WEAR </a>
+          <a href='https://shopiccon.com/collections/sale'>SALE </a>
+        </ul>
+       
+      </nav>
+        
+        </header>
+        </div>
+    
+    
+  )
+}
